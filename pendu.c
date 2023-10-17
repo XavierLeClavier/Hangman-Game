296da427e1c353689ff20bc58aac153619e11884
Xavi <xavier.stoa@gmail.com>
2023-10-17 08:36:30 +0200

    Added life system, the program now prints the found letters, it also finds when all the letters are found. However it doesnt stop when the letters are found

--- pendu.c
+++ pendu.c
@@ -6,14 +6,14 @@
 
 int main() {
     int lives;
-    bool foundword;
-    char words[][10] = {"tissu", "tranche", "grele", "masse", "dessin", "portable", "trompette", "page", "fourmi", "cartouche", "pain"};
+    bool foundword, foundthisturn;
+    char words[5][10] = {"tissu", "tranche", "grele", "masse", "dessin", "portable", "trompette", "page", "fourmi", "cartouche", "pain"};
     char *list_of_words[5];
-    char tissu[] = {'t', 'i', 's', 's', 'u', '\0'};
-    char tranche[] = {'t', 'r', 'a', 'n', 'c', 'h', 'e', '\0'};
-    char grele[] = {'g', 'r', 'e', 'l', 'e', '\0'};
-    char masse[] = {'m', 'a', 's', 's', 'e', '\0'};
-    char dessin[] = {'d', 'e', 's', 's', 'i', 'n', '\0'};
+    char tissu[6][1] = {'t', 'i', 's', 's', 'u', '\0'};
+    char tranche[8][1] = {'t', 'r', 'a', 'n', 'c', 'h', 'e', '\0'};
+    char grele[6][1] = {'g', 'r', 'e', 'l', 'e', "\0"};
+    char masse[6][1] = {'m', 'a', 's', 's', 'e', '\0'};
+    char dessin[7][1] = {'d', 'e', 's', 's', 'i', 'n', '\0'};
 
     list_of_words[0] = tissu;
     list_of_words[1] = tranche;
@@ -29,32 +29,71 @@ int main() {
     random_num = rand() % 5; // generate a random number between 0 and 4
     char *divword = list_of_words[random_num];
     char *word = words[random_num];
+    char letter;
+    int length = strlen(list_of_words[random_num]);
+    char wordadvancement[length];//creates a lists that will start empty and gradually fill with letters
+    bool foundletters[length];//tells if the letter related to a given indexnis found
 
-    while (foundword == false && lives > 0) {
-        int length = strlen(list_of_words[random_num]);
-        char wordadvancement[length];
-        bool foundletters[length];
-        for (int i = 0; i < length; i++) {
-            foundletters[i] = false;
+    for (int i = 0; i < length; i++) 
+    {
+        foundletters[i] = false;
+    }
+    while (foundword == false && lives > 0) 
+    {
+        //this loops until either the word is found or lives have ran out
+        printf("%s \nInsérez une lettre: ", wordadvancement);
+        scanf(" %s", &letter);
+        printf("%s \n", word);
+        for (int i = 0; i < length; i++) 
+        {
+            if (divword[i] == letter||divword[i]=='\0') 
+            {
+                foundletters[i] = true;
+                foundthisturn = true;
+            }
         }
-        for (int i = 0; i < length; i++) {
-            if (foundletters[i] == true){
+        if (foundthisturn == false)
+        {
+            lives = lives-1;
+        }
+
+        //the advancement is always recalculated
+        for (int i = 0; i < length; i++) 
+        {
+            if (foundletters[i] == true)
+            {
                 wordadvancement[i] = divword[i];
             }
-            else if (foundletters[i] == false){
+            else if (foundletters[i] == false)
+            {
                 wordadvancement[i] = '_';
             }
         }
-
-        printf("%c \nInsérez une lettre: ", wordadvancement);
-        char letter;
-        scanf(" %c", &letter);
-        for (int i = 0; i < length; i++) {
-            if (divword[i] == letter) {
-                foundletters[i] = true;
+        foundword=true;
+        while (foundword == true) //this does not stop once completed!!!!!
+        {
+            for(int i=0; i<length; i++)
+            {
+                if (foundletters[i] == false)
+                {
+                    foundword=false;
+                }
             }
-       
         }
-    return 0;
-}
+
+        foundthisturn=false;
+        printf("end turn");
+    }
+
+    printf("fin de la boucle");
+    if (lives <= 0)
+    {
+        printf("Vous avez perdu");
+    }
+    else
+    {
+        printf("Bravo vous avez gagné");
+    }
+ 
+return 0;
 }
